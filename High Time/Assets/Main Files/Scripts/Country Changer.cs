fda512c3c95bd80c92af38c8ee13ca8fe97c6a20
ashishnair436 <ashishnair436@gmail.com>
2022-10-11 12:55:48 +0530

    Fifth Commit

---
+++ High Time/Assets/Main Files/Scripts/Country Changer.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public void US()
+    {
+            SceneManager.LoadScene(3);
+        
+    }
+
+    public void Japan()
+    {
+        SceneManager.LoadScene(4);
+
+    }
+
+    public void India()
+    {
+        SceneManager.LoadScene(2);
+
+    }
+
+    public void EndCredits()
+    {
+        SceneManager.LoadScene(5);
+
+    }
+
+    public void MapLoad()
+    {
+        SceneManager.LoadScene(1);
+
+    }
+
+
+
+          
+}
